a18c101e1932ce4b5a181a1d962d66fefc7672e9
MagdalenaSiegel <68266700+MagdalenaSiegel@users.noreply.github.com>
2020-08-31 12:08:25 +0200

    update report

--- params_report.R
+++ params_report.R
@@ -11,7 +11,7 @@ params <- list(
                   "logOR" = "Odds Ratio (Log-Scale)"),
   k = length(data_reac$DT[[para$es]]),
   k_pub = NA,
-  n = data_reac$DT[[para$es]],
+  n = data_reac$DT[[para$n]],
   n_pub = NA,
   in_study = para$prim,
   in_study_pub = "Author et al (2001)",
@@ -60,11 +60,47 @@ params <- list(
   # ** Begg & Mazumdar ----
   pb_bm_tau = BMres$res$tau,
   pb_bm_pval = BMres$res$pval,
+  thres_bm_pval = input$BM_p,
+  
+  # ** Sterne & Egger ----
   pb_se_zval = SEres$res$zval,
   pb_se_pval = SEres$res$pval,
+  thres_se_pval = input$SE_p,
+  
+  # ** Trim-and-Fill ----
   pb_tf_k = TFres$res$k0,
   pb_tf_side = TFres$res$side,
   pb_tf_es = TFres$res$beta,
+  thres_tf_adj = input$tf_adj,
+  
+  # ** Pcurve ----
   pb_pcurve_restab = pcurve_table(),
-  pb_pcurve_powertab = pcurvePower()
+  pb_pcurve_powertab = pcurvePower(),
+  
+  # ** Puniform and Puniform* ----
+  pb_puni_zval = PUNIres$res$L.pb,
+  pb_puni_pval = PUNIres$res$pval.pb,
+  thres_puni_pval = input$puni_p,
+  pb_punistar_zval = PUNISTres$res$L.pb,
+  pb_punistar_pval = PUNISTres$res$pval.pb,
+  thres_punistar_pval = input$punistar_p,
+  
+  # TES ----
+  pb_tes_o = TESres$O,
+  pb_tes_e = TESres$E,
+  pb_tes_res = TESres$A,
+  pb_tes_pval = TESres$res,
+  thres_tes_pval = input$TES_p,
+  
+  # Selection Models ----
+  pb_sel_mod1 = SelMods$mod1$output_adj$par[2],
+  pb_sel_sev1 = SelMods$sev1$output_adj$par[2],
+  pb_sel_mod2 = SelMods$mod2$output_adj$par[2],
+  pb_sel_sev2 = SelMods$sev2$output_adj$par[2],
+  pb_sel_unadj = SelMods$mod1$output_unadj$par[2],
+  pb_sel_perc_mod1 = 1 / SelMods$mod1$output_unadj$par[2] * (SelMods$mod1$output_unadj$par[2] - SelMods$mod1$output_adj$par[2]),
+  pb_sel_perc_sev1 = 1 / SelMods$sev1$output_unadj$par[2] * (SelMods$sev1$output_unadj$par[2] - SelMods$sev1$output_adj$par[2]),
+  pb_sel_perc_mod2 = 1 / SelMods$mod2$output_unadj$par[2] * (SelMods$mod2$output_unadj$par[2] - SelMods$mod2$output_adj$par[2]),
+  pb_sel_perc_sev2 = 1 / SelMods$sev2$output_unadj$par[2] * (SelMods$sev2$output_unadj$par[2] - SelMods$sev2$output_adj$par[2]),
+  thres_sel_adj = input$sel_adj
 )
